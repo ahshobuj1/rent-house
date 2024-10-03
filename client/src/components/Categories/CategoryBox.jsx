b32bf914d3479d7e24e3774780612ed27f7aa740
A H Shobuj <ahshobuj09@gmail.com>
2024-10-03 17:52:18 +0600

    Set and Get query to url: Parse and stringify URL query strings

--- client/src/components/Categories/CategoryBox.jsx
+++ client/src/components/Categories/CategoryBox.jsx
@@ -1,28 +1,48 @@
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
+import queryString from 'query-string';
+import {useNavigate, useSearchParams} from 'react-router-dom';
 
-const CategoryBox = ({ label, icon: Icon }) => {
-  return (
-    <div
-      className={`flex 
+const CategoryBox = ({label, icon: Icon}) => {
+    const navigate = useNavigate();
+
+    //* Get query to url: Parse and stringify URL query strings
+    const [params] = useSearchParams();
+    const category = params.get('category');
+    console.log(category);
+
+    const handleClick = () => {
+        //* Set query to url: Parse and stringify URL query strings
+        const currentCategory = {category: label};
+        const url = queryString.stringifyUrl({
+            url: '/',
+            query: currentCategory,
+        });
+        navigate(url);
+        //console.log(url);
+    };
+
+    return (
+        <div
+            onClick={handleClick}
+            className={`flex 
   flex-col 
   items-center 
-  justify-center 
+  justify-center  
   gap-2
   p-3
   border-b-2
   hover:text-neutral-800
   transition
-  cursor-pointer`}
-    >
-      <Icon size={26} />
-      <div className='text-sm font-medium'>{label}</div>
-    </div>
-  )
-}
+  cursor-pointer ${category === label && 'border-b-red-600 text-green-600'}`}>
+            <Icon size={26} />
+            <div className="text-sm font-medium">{label}</div>
+        </div>
+    );
+};
 
 CategoryBox.propTypes = {
-  label: PropTypes.string,
-  icon: PropTypes.elementType,
-}
+    label: PropTypes.string,
+    icon: PropTypes.elementType,
+};
 
-export default CategoryBox
+export default CategoryBox;
--- client/src/components/Home/Rooms.jsx
+++ client/src/components/Home/Rooms.jsx
@@ -1,44 +1,50 @@
-import { useEffect, useState } from 'react'
-import Card from './Card'
-import Container from '../Shared/Container'
-import Heading from '../Shared/Heading'
-import LoadingSpinner from '../Shared/LoadingSpinner'
+import {useEffect, useState} from 'react';
+import Card from './Card';
+import Container from '../Shared/Container';
+import Heading from '../Shared/Heading';
+import LoadingSpinner from '../Shared/LoadingSpinner';
+import {useSearchParams} from 'react-router-dom';
 
 const Rooms = () => {
-  const [rooms, setRooms] = useState([])
-  const [loading, setLoading] = useState(false)
+    const [rooms, setRooms] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setLoading(true)
-    fetch(`./rooms.json`)
-      .then(res => res.json())
-      .then(data => {
-        setRooms(data)
-        setLoading(false)
-      })
-  }, [])
+    //* Get query to url: Parse and stringify URL query strings
+    const [params] = useSearchParams();
+    const category = params.get('category');
+    console.log(category);
 
-  if (loading) return <LoadingSpinner />
+    useEffect(() => {
+        setLoading(true);
+        fetch(`./rooms.json`)
+            .then((res) => res.json())
+            .then((data) => {
+                setRooms(data);
+                setLoading(false);
+            });
+    }, []);
 
-  return (
-    <Container>
-      {rooms && rooms.length > 0 ? (
-        <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-          {rooms.map(room => (
-            <Card key={room._id} room={room} />
-          ))}
-        </div>
-      ) : (
-        <div className='flex items-center justify-center min-h-[calc(100vh-300px)]'>
-          <Heading
-            center={true}
-            title='No Rooms Available In This Category!'
-            subtitle='Please Select Other Categories.'
-          />
-        </div>
-      )}
-    </Container>
-  )
-}
+    if (loading) return <LoadingSpinner />;
 
-export default Rooms
+    return (
+        <Container>
+            {rooms && rooms.length > 0 ? (
+                <div className="pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+                    {rooms.map((room) => (
+                        <Card key={room._id} room={room} />
+                    ))}
+                </div>
+            ) : (
+                <div className="flex items-center justify-center min-h-[calc(100vh-300px)]">
+                    <Heading
+                        center={true}
+                        title="No Rooms Available In This Category!"
+                        subtitle="Please Select Other Categories."
+                    />
+                </div>
+            )}
+        </Container>
+    );
+};
+
+export default Rooms;
