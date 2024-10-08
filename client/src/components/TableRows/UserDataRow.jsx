d54242d51e24b146d59c4be315163cf54b35a583
A H Shobuj <ahshobuj09@gmail.com>
2024-10-08 22:11:21 +0600

    create dropdown modaal to update user role

--- client/src/components/TableRows/UserDataRow.jsx
+++ client/src/components/TableRows/UserDataRow.jsx
@@ -1,6 +1,14 @@
 import PropTypes from 'prop-types';
 import LoadingSpinner from '../Shared/LoadingSpinner';
+import UpdateUserModal from '../Modal/UpdateUserModal';
+import {useState} from 'react';
 const UserDataRow = ({user, refetch, isPending}) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const modalHandler = (selected) => {
+        console.log('selected user role -->>', selected);
+    };
+
     if (isPending) {
         return <LoadingSpinner />;
     }
@@ -19,9 +27,9 @@ const UserDataRow = ({user, refetch, isPending}) => {
                 {user?.status ? (
                     <p
                         className={`${
-                            user.status === 'Verified'
+                            user.status === 'Normal'
                                 ? 'text-green-500'
-                                : 'text-yellow-500'
+                                : 'text-red-500'
                         } whitespace-no-wrap`}>
                         {user.status}
                     </p>
@@ -33,13 +41,21 @@ const UserDataRow = ({user, refetch, isPending}) => {
             </td>
 
             <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                <span className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                <button
+                    onClick={() => setIsOpen(true)}
+                    className="relative cursor-pointer inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
                     <span
                         aria-hidden="true"
-                        className="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
+                        className="absolute inset-0 bg-green-200 opacity-50 rounded-lg"></span>
                     <span className="relative">Update Role</span>
-                </span>
+                </button>
                 {/* Update User Modal */}
+                <UpdateUserModal
+                    isOpen={isOpen}
+                    setIsOpen={setIsOpen}
+                    modalHandler={modalHandler}
+                    user={user}
+                />
             </td>
         </tr>
     );
